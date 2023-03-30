ecd6f3718a74804bc688fbf0f70f79626e6d547a
Marco Antônio <ma.souza.junior@gmail.com>
2023-03-30 19:01:31 -0300

    enh: improve context generation
    - get initial and end context from the same file

--- src/history.rs
+++ src/history.rs
@@ -10,7 +10,7 @@ const INITIAL_PROMPT: &str = concat!(
     "AI: 3 + 2 = 5\n",
 );
 const HISTORY_FILE: &str = ".openai-history";
-const CONTEXT_SIZE: usize = 200;
+const CONTEXT_SIZE: usize = 100;
 
 pub struct History {
     file: String,
@@ -33,9 +33,11 @@ impl History {
         let history = self.history();
         match history.len() {
             // limit context size
-            size if size > self.context_size => {
-                let offset = history.len() - CONTEXT_SIZE;
-                history[offset..].to_string()
+            size if size > self.context_size * 2 => {
+                let offset = history.len() - self.context_size;
+                let init = history[..offset].to_string();
+                let end = history[offset..].to_string();
+                init + &end
             }
             _ => history,
         }
