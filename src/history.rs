a6a6d9742d6896bff43a6aff03ff30b281b08e46
Marco Antônio <ma.souza.junior@gmail.com>
2023-03-30 18:18:06 -0300

    fea: create module for history
    - create module for history
    - add history to main.rs
    - create utils for managing context

---
+++ src/history.rs
@@ -0,0 +1,62 @@
+use std::{env, fs, path::PathBuf};
+
+const INITIAL_PROMPT: &str = concat!(
+    "Act as my personal AI Assistent. ",
+    "You will assist me in my daily tasks.\n",
+    "The assistant is helpful, creative, clever, and very friendly.\n\n",
+    "Human: Hello, who are you?\n",
+    "AI: I am an AI created by OpenAI. How can I help you today?\n",
+    "Human: Can you calculate how much is 3+2?\n",
+    "AI: 3 + 2 = 5\n",
+);
+const HISTORY_FILE: &str = ".openai-history";
+const CONTEXT_SIZE: usize = 200;
+
+pub struct History {
+    file: String,
+    fallback: String,
+    context_size: usize,
+}
+
+impl History {
+    pub fn new() -> Self {
+        let home_dir = env::var("HOME").expect("HOME env var not set");
+        Self {
+            file: format!("{}/{}", home_dir, HISTORY_FILE),
+            fallback: INITIAL_PROMPT.to_string(),
+            context_size: CONTEXT_SIZE,
+        }
+    }
+
+    pub fn context(&self) -> String {
+        // load history from file
+        let history = self.history();
+        match history.len() {
+            // limit context size
+            size if size > self.context_size => {
+                let offset = history.len() - CONTEXT_SIZE;
+                history[offset..].to_string()
+            }
+            _ => history,
+        }
+    }
+
+    pub fn persist(&self, input: String, output: String) {
+        let path = PathBuf::from(&self.file);
+        // Prepare context for file
+        let context = format!("Human: {}\nAI: {}\n", input, output);
+        let history = format!("{}{}", self.history(), context);
+
+        fs::write(path.clone(), history).expect("Unable to write file");
+    }
+
+    fn history(&self) -> String {
+        match fs::read_to_string(self.file.clone()) {
+            Ok(history) => match history.len() {
+                0 => self.fallback.clone(),
+                _ => history,
+            },
+            Err(_) => self.fallback.clone(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
-use tokio;
-use dotenv::dotenv;
-
+mod history;
 mod openai;
 
-fn setup() {
-    // Load environment variables from the .env file
-    dotenv().ok();
-}
+use history::History;
+use tokio;
 
 #[tokio::main]
 async fn main() {
-    setup();
-
+    let history = History::new();
     let client = openai::OpenAIClient::new();
-    // get prompt merging all agrs
-    let args = std::env::args();
-    let prompt = args.skip(1).collect::<Vec<String>>().join(" ");
-    let role = concat!(
-        "The following is a conversation with an AI assistant. ",
-        "The assistant is helpful, creative, clever, and very friendly.\n\n",
-        "Human: Hello, who are you?\n",
-        "AI: I am an AI created by OpenAI. How can I help you today?\n",
-        "Human: ",
-    );
-    let prompt = format!("{}{}\n", role, prompt);
 
-    let text = client.prompt(prompt.clone()).await.unwrap();
-    println!("{}", text);
+    // get prompt merging all agrs
+    let input = get_cli_input();
+    let context = history.context();
+    let prompt = format!("{}{}\nAI: ", context, input);
+
+    let output = client.prompt(prompt).await.unwrap();
+    println!("{}", output);
+
+    history.persist(input, output);
+}
+
+fn get_cli_input() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    args[1..].join(" ")
 }
